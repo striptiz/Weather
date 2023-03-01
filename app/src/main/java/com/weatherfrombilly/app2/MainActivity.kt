e127eb0915ebb839a6056ff4e83381963c35c2c5
nopbreak <nopbreak@gmail.com>
2023-03-01 11:30:13 +0300

    snapshot

--- app/src/main/java/com/weatherfrombilly/app2/MainActivity.kt
+++ app/src/main/java/com/weatherfrombilly/app2/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.weatherfrombilly.app2
 
 import android.os.Bundle
+import android.view.View
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
@@ -10,27 +11,93 @@ import com.weatherfrombilly.app2.ui.main.viewmodel.UiState
 
 class MainActivity : AppCompatActivity() {
     private lateinit var viewModel: MainViewModel
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
         viewModel = ViewModelProvider(this)[MainViewModel::class.java]
         binding.model = viewModel
         binding.lifecycleOwner = this
         viewModel.state.observe(this) { state ->
-            if (state == UiState.LOADED) {
-                binding.swiperefresh.isRefreshing = false
-            }
+            renderState(state)
         }
 
-        binding.swiperefresh.setOnRefreshListener {
+        binding.swipeRefresh.setOnRefreshListener {
             viewModel.onRefresh()
+        }
+    }
+
+    private fun renderState(state: UiState) {
+        when (state) {
+            UiState.ERROR -> {
+                hideMain()
+                showError()
+                binding.swipeRefresh.isRefreshing = false
+                hide(binding.activityMainProgress)
+            }
+            UiState.LOADED -> {
+                showMain()
+                binding.swipeRefresh.isRefreshing = false
+                hide(binding.activityMainProgress)
+            }
+            UiState.LOADING -> {}
+            UiState.START -> {
+                hideMain()
+                show(binding.activityMainProgress)
+            }
+        }
+    }
+
+    private fun showError() {
+    }
+
+    private fun showMain() {
+        binding.apply {
+            show(
+                cityNameTV,
+                weatherIconIV,
+                weatherDescTV,
+                currentTempTV,
+                tempIV,
+                rainIV,
+                currentRainIntencityTV,
+                windIconIV,
+                currentWindSpeedTV,
+                activityMainProgress
+            )
+        }
+    }
+
+    private fun hideMain() {
+        binding.apply {
+            hide(
+                cityNameTV,
+                weatherIconIV,
+                weatherDescTV,
+                currentTempTV,
+                tempIV,
+                rainIV,
+                currentRainIntencityTV,
+                windIconIV,
+                currentWindSpeedTV,
+                activityMainProgress
+            )
         }
     }
 
     override fun onResume() {
         super.onResume()
         viewModel.onResume()
+    }
+
+    companion object {
+        private fun show(vararg views: View) {
+            views.forEach { it.visibility = View.VISIBLE }
+        }
+
+        private fun hide(vararg views: View) {
+            views.forEach { it.visibility = View.GONE }
+        }
     }
 }
--- app/src/main/java/com/weatherfrombilly/app2/data/model/gis/CurrentWeatherResponse.kt
+++ app/src/main/java/com/weatherfrombilly/app2/data/model/gis/CurrentWeatherResponse.kt
@@ -1,7 +1,5 @@
 package com.weatherfrombilly.app2.data.model.gis
 
-import com.weatherfrombilly.app2.data.model.CurrentWeatherResponseData
-
 data class CurrentWeatherResponse(
     val errorCode: Int,
     val data: CurrentWeatherData
